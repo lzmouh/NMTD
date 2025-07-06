840eda499b5babd9dd9e9d3057fef9882e4bddd4
lzmouh <lzmouh@gmail.com>
2025-07-06 12:16:01 +0300

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -4,6 +4,8 @@ from config import fluid_impedance_db, default_densities, DEFAULT_CONFIG
 
 def show_simulator():
     st.title("🔍 NMTD Ultrasonic Response Simulator")
+
+    config = st.session_state["config"]
     # --- Initialize default config if missing ---
     if "config" not in st.session_state or not st.session_state["config"]:
         st.session_state["config"] = DEFAULT_CONFIG.copy()
