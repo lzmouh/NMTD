c90db15a2ea205ba73d92819073c43c7783a9a5f
lzmouh <lzmouh@gmail.com>
2025-07-08 17:05:12 +0300

    Update plots.py

--- plots.py
+++ plots.py
@@ -153,7 +153,7 @@ def show_plots():
     st.subheader("🔴 Pulse-Compressed A-Scan")
     fig2 = go.Figure()
     fig2.add_trace(go.Scatter(x=t_rx*1e6, y=compressed, line=dict(color="firebrick"), name="Compressed"))
-    for _, row in df.iterrows():
+    for _, row in df_mode1.iterrows():
         fig2.add_vline(x=row["Time (µs)"], line_dash="dot", line_color="gray",
                        annotation_text=row["Layer"], annotation_position="top right")
     fig2.update_layout(xaxis_title="Time (µs)", yaxis_title="Amp",
