39047968c1054ab493a88286488b702d1f869298
lzmouh <lzmouh@gmail.com>
2025-07-10 11:25:07 +0300

    Update about.py

--- about.py
+++ about.py
@@ -17,5 +17,5 @@ This tool aids design, development, and early feasibility testing of ultrasonic-
 
 ### Developed by: 
 **Mohamed LARBI ZEGHLACHE**
-EXPEC-ARC / PTD / AWSFA - 2025
+###EXPEC-ARC / PTD / AWSFA - 2025
 """)
