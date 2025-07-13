4ac34b5ab459c406c25595002db6c1cb3f0aa9ff
lzmouh <lzmouh@gmail.com>
2025-07-13 10:57:51 +0000

    Update plots.py

--- plots.py
+++ plots.py
@@ -121,6 +121,9 @@ def show_plots():
     config = st.session_state["config"]
     fs = config["sampling_rate"]
 
+    t_tx = np.array(config["tx_chirp_t"])
+    tx = np.array(config["tx_chirp_waveform"])
+ 
     # 1) Display chirp signal
     col1, col2 = st.columns(2)
     with col1:
